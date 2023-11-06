f918896442e02ee77df0a9c1e44f8e59045e2b44
Matthew-Lund <mt.lund21@gmail.com>
2023-11-06 12:11:47 -0500

    Added Comments

--- DLP Laptop.py
+++ DLP Laptop.py
@@ -1,7 +1,8 @@
-from math import ceil, sqrt
-import time
-from multiprocessing import Pool
+# Matthew Lund mtlund@wpi.edu
+#DLP Challenge Baby-Step Giant-Step Algorithm
 
+from math import ceil, sqrt #for math in bsgs
+import time #for outputting execution time
 
 #modular exponention expression
 def mod_exp(base, exp, mod):
@@ -16,18 +17,35 @@ def mod_exp(base, exp, mod):
 
     return result
 
-def bs_gs(a, b, p, N, tbl):
+def square_and_multiply(base, exponent, modulus):
+    result = 1
+    base = base % modulus
+
+    while exponent > 0:
+        # If exponent is odd, multiply result with base
+        if exponent % 2 == 1:
+            result = (result * base) % modulus
+
+        # Square base and reduce exponent by half
+        base = (base * base) % modulus
+        exponent = exponent // 2
+
+    return result
+
+def bs_gs(a, b, p):
+
+    N = int(ceil(sqrt(p - 1)))  # phi(p) is p-1 if p is prime
+
+    # Store hashmap of g^{1...m} (mod p). Baby step.
+    tbl = {pow(a, i, p): i for i in range(N)}
 
     # Precompute via Fermat's Little Theorem
-    c = pow(a, N * (p - 2), p)
+    c = square_and_multiply(a, N * (p - 2), p)
 
-    def pow_mod(base, exp): #to call mod_exp in here
-        return mod_exp(base, exp, p)
-    
     # Search for an equivalence in the table. Giant step.
     for j in range(N):
-        y = (b * pow_mod(c,j)) % p
-        if y in tbl:
+        y = (b * mod_exp(c,j,p)) % p    #modular exponention for y
+        if y in tbl:    #check for y
             return j * N + tbl[y]
 
 
@@ -36,18 +54,14 @@ def bs_gs(a, b, p, N, tbl):
     
 # Solve for x
 
-
 #40 bits implementation
 a = 3
 b = 1228035139812
 p = 2199023255867
-N = int(ceil(sqrt(p - 1)))  # phi(p) is p-1 if p is prime
 
-# Store hashmap of g^{1...m} (mod p). Baby step.
-tbl = {pow(a, i, p): i for i in range(N)}
-print(a, b, p)
+print('a: ', a, 'B: ', b,'P: ', p)
 start = time.time()
-x = bs_gs(a,b,p, N, tbl)
+x = bs_gs(a,b,p)
 execution_time = time.time()
 elapsed = execution_time - start
 print(x)

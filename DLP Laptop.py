ffcc7dd4c8cf259b0dc4a9e133a4c1af5ad35d64
Matthew-Lund <mt.lund21@gmail.com>
2023-11-06 19:44:21 -0500

    Updated Code with Memory Benchmarking and utilizing numpy numbers

--- DLP Laptop.py
+++ DLP Laptop.py
@@ -3,6 +3,9 @@
 
 from math import ceil, sqrt #for math in bsgs
 import time #for outputting execution time
+import psutil #for looking @ memory usage
+import os   #for accessing memory usage
+import numpy #for addressing possible memory leak issues in table to limit to 32 bit (sqrt(P of 60 bits) = 30 bits)
 
 #modular exponention expression
 def mod_exp(base, exp, mod):
@@ -33,11 +36,18 @@ def square_and_multiply(base, exponent, modulus):
     return result
 
 def bs_gs(a, b, p):
+    process = psutil.Process(os.getpid())
+    base_memory_usage = process.memory_info().rss
 
-    N = int(ceil(sqrt(p - 1)))  # phi(p) is p-1 if p is prime
+    N = numpy.uint32(ceil(sqrt(p - 1)))  # phi(p) is p-1 if p is prime
 
-    # Store hashmap of g^{1...m} (mod p). Baby step.
-    tbl = {pow(a, i, p): i for i in range(N)}
+    # Store hashmap of a^{1...N} (mod p). Baby step.
+    tbl = {numpy.uint64(mod_exp(a, i, p)): numpy.uint32(i) for i in range(N)}
+
+    #Report RAM utilization of Hashmap
+    memory_usage = process.memory_info().rss
+    table_memory_usage = memory_usage - base_memory_usage
+    print((table_memory_usage * (10**-6)), 'MB of RAM used for Table Execution')
 
     # Precompute via Fermat's Little Theorem
     c = square_and_multiply(a, N * (p - 2), p)
@@ -52,7 +62,7 @@ def bs_gs(a, b, p):
     # Solution not found
     return None
     
-# Solve for x
+# Implementations
 
 #40 bits implementation
 a = 3
@@ -64,23 +74,21 @@ start = time.time()
 x = bs_gs(a,b,p)
 execution_time = time.time()
 elapsed = execution_time - start
-print(x)
+print('X =',x)
 print('Execution Time (seconds):', elapsed)
 
-'''
+print() #new line
+
 #60 bits implementation
 a = 3
 b = 259893785866906004
 p = 2305843009213699919
 print(a, b, p)
 start = time.time()
-N = int(ceil(sqrt(p - 1)))  # phi(p) is p-1 if p is prime
-tbl = [pow(a, i, p): i for i in range(N)]
-print(a, b, p)
+print('a: ', a, 'B: ', b,'P: ', p)
 start = time.time()
-x = bs_gs(a,b,p, N, tbl)
+x = bs_gs(a,b,p)
 execution_time = time.time()
 elapsed = execution_time - start
-print(x)
+print('X =',x)
 print('Execution Time (seconds):', elapsed)
-'''
